4a47a792c709ecd0d00bb65e9a5c55b979eccdf4
marcevrard <mevdevmev@gmail.com>
2014-11-06 15:57:32 +0100

    Setup py_calliphony.py

---
+++ py_calliphony.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+"""
+SYNOPSIS
+
+    py_calliphony [-h,--help] [-v,--verbose] [--version]
+
+DESCRIPTION
+
+    **TODO** This describes how to use this script. This docstring
+        will be printed by the script if there is an error or
+        if the user requests help (-h or --help).
+
+EXAMPLES
+
+    %run /Volumes/Python/pyCalliphony/py_calliphony.py
+
+EXIT STATUS
+
+    **TODO** List exit codes
+
+AUTHORS
+
+    Marc Evrard             <marc.evrard@limsi.fr>
+
+LICENSE
+
+    This script is in the public domain, free from copyrights or restrictions.
+
+VERSION
+
+    $Id$
+"""
